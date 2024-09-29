4e57d76ff009e946d06a24bb855ce2b36b478674
archita172004 <archichaudhary2004@gmail.com>
2024-09-30 00:02:49 +0530

    databse connected and model made

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+
+//connectiong database
+mongoose
+  .connect(`${process.env.MONGOOSE_URL}`, {
+    dbName: "backend2",
+  })
+  .then(() => console.log("Database Connected"))
+  .catch((e) => console.log(e));
+
+const messageSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+//collection name Message
+const Message = mongoose.model("Message", messageSchema);
 
 const users = [];
 
@@ -10,6 +28,12 @@ app.use(middlewareStatic);
 //using middlewares
 app.use(express.urlencoded({ extended: true }));
 
+// app.get("/add", async (req, res) => {
+//   await Message.create({ name: "Archita2", email: "sample2@gmail.com" });
+
+//   res.send("nice");
+// });
+
 //setting up view engine
 app.set("view engine", "ejs");
 
@@ -18,16 +42,14 @@ app.get("/", (req, res) => {
   res.render("index", { name: "Archita" });
 });
 
-//rendering succes page in view
+//rendering success page in view
 app.get("/success", (req, res) => {
   res.render("success");
 });
 
 //handling the form submission
-app.post("/contact", (req, res) => {
-  users.push({ userName: req.body.name, email: req.body.email });
-
-  // res.render("success");
+app.post("/contact", async (req, res) => {
+  await Message.create({ name: req.body.name, email: req.body.email });
   res.redirect("/success");
 });
 
